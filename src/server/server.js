08d853e20fc64c09e4e745d5a73e78540ce6c2be
Zakaria Yaris <info@zakariya.me>
2023-05-23 15:34:33 +0100

    basic home page styling setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,27 +1,35 @@
 const express = require('express');
-const { home, board } = require('template.js');
+const { home, board } = require('./template');
 const server = express();
 
 server.use(express.static('public'));
 
 server.get('/', (req, res) => {
-    res.send(home());
-  });
+  res.send(home());
+});
 
-const users = ['Taha', 'Mark', 'Cameo', 'Zak', 'Simon', 'Beth', 'Alphonso', 'Thom']
+const users = [
+  'Taha',
+  'Mark',
+  'Cameo',
+  'Zak',
+  'Simon',
+  'Beth',
+  'Alphonso',
+  'Thom',
+];
 
 server.get('/board/:name', (req, res) => {
   const name = req.params.name;
-  if (users.includes(name)){
+  if (users.includes(name)) {
     res.send(board(name));
-  }
-  else {
+  } else {
     res.redirect('/');
   }
-})
+});
 
 server.post('/post', (req, res) => {
-  res.redirect('/board')
-})
+  res.redirect('/board');
+});
 
-  module.exports = server;
+module.exports = server;
