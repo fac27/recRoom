503d6a1a6bc48e37084844c68018cfe6646faf92
cameochoquer <cjchoquer@gmail.com>
2023-05-23 17:37:49 +0100

    expanded function and added example of server post

--- model/delete.js
+++ model/delete.js
@@ -1,11 +1,21 @@
 const db = require('../src/database/db.js')
 
-const delete_post = db.prepare(/*sql/`
+const delete_post = db.prepare(/*sql*/`
 DELETE FROM posts where id = $id
 `);
 
-const postToDelete = ${postId}
 
-const deletePost = () =>{
-return delete_post.run({id: postToDelete});
+const deletePost = (postId) =>{
+    return delete_post.run({id: postId});
 };
+
+// example of what it *could* look like on server side? 
+server.post('/delete/:post', (req, res) => {
+    const { postId } = req.params;
+    const index = posts.findIndex((post) => post.id === id);
+    if (index !== -1) {
+        deletePost(postId);
+        posts.splice(index, 1);
+        savePosts(posts);
+    res.redirect('/');}
+  });
