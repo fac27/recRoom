9fd9b19e4f792ef4914eaca390d84fa8c1359261
cameochoquer <cjchoquer@gmail.com>
2023-05-23 15:00:30 +0100

    nietes help

--- src/database/seed.sql
+++ src/database/seed.sql
@@ -1,31 +1,27 @@
 BEGIN;
 
-INSERT INTO users(name)VALUES
-('mark')
-('taha')
-('zack')
-('cameo')
-('alphonso')
-('beth')
-('simon')
-('tom')
+INSERT INTO users(id, name)VALUES
+(1, 'mark'),
+(2, 'taha'),
+(3, 'zack'),
+(4, 'cameo'),
+(5, 'alphonso'),
+(6, 'beth'),
+(7, 'simon'),
+(8, 'tom')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO posts(artist, song, user_id,)VALUES
-('elvis presley', 'jailhouse rock')
-('dolly parton', 'jolene')
-('tool', 'schism')
-('britney spears', 'toxic')
+INSERT INTO posts(id, artist, song, user_id,)VALUES
+(1, 'elvis presley', 'jailhouse rock', 3),
+(2, 'dolly parton', 'jolene', 4),
+(3, 'tool', 'schism', 5),
+(4, 'britney spears', 'toxic', 6)
 ON CONFLICT DO NOTHING;
 
 INSERT INTO ratings(rating, user_id, post_id)VALUES
-(1)
-(-1)
-(1)
-(-1)
+(1, 3, 1),
+(1, 4, 2),
+(1, 5, 3),
+(1, 6, 4)
 ON CONFLICT DO NOTHING;
-
-
 COMMIT;
--- is this necessary? 
-PRAGMA foreign_keys = ON;
