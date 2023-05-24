e98cc58497df675ded87a0bc22f98d7e233020f4
Mark hanley <home@markhanley.co.uk>
2023-05-24 14:38:41 +0100

    Send js object in  http response in createPost

--- src/server/server.js
+++ src/server/server.js
@@ -1,53 +1,60 @@
-const express = require('express');
-const { getAllPosts, createPost } = require('../database/model/posts');
-const { home, board } = require('./template');
+const express = require("express");
+const { getAllPosts, createPost } = require("../database/model/posts");
+const { home, board } = require("./template");
 const server = express();
 
-server.use(express.static('public'));
+server.use(express.static("public"));
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(home());
 });
 
 const users = [
-  'taha',
-  'mark',
-  'cameo',
-  'zak',
-  'simon',
-  'beth',
-  'alphonso',
-  'thom',
+  "taha",
+  "mark",
+  "cameo",
+  "zak",
+  "simon",
+  "beth",
+  "alphonso",
+  "thom",
 ];
 
-server.get('/board/:name', async (req, res) => {
+server.get("/board/:name", async (req, res) => {
   const name = req.params.name;
   if (users.includes(name)) {
     const posts = await getAllPosts();
     res.send(board(name, posts));
   } else {
-    res.redirect('/');
+    res.redirect("/");
   }
 });
 
-server.get('/test', async (req, res) => {
+server.get("/test", async (req, res) => {
   const posts = await getAllPosts();
-  res.send(posts)
+  res.send(posts);
 });
 
-server.post("/post", express.urlencoded({ extended: false }), async (req, res) => {
+server.post(
+  "/post",
+  express.urlencoded({ extended: false }),
+  async (req, res) => {
+    const { user_id, artist, song, spotify_url } = req.body;
+    console.log(`Adding recommendation by ${user_id} (${song} by ${artist})`);
 
-  const { user_id, artist, song, url } = req.body;
-  console.log(`Adding recommendation by ${user_id} (${song} by ${artist})`);
+    const post_id = await createPost({ user_id, artist, song, spotify_url });
 
-  const post_id = await createPost ({user_id, artist, song, url});
-
-  if (post_id) {
-    res.status(201).send(`Post created with id: ${typeof post_id}`);
+    if (post_id) {
+      res.status(201).send({
+        message: `Post created with id: ${post_id}`,
+        redirect: "/",
+      });
+    } else {
+      res.status(400).send({
+        message: `Error creating post`,
+        redirect: "/" });
+    }
   }
-  else {
-    res.status(400).send('Post failed');
-  }
-});
+);
 
 module.exports = server;
