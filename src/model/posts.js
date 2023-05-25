effbbdb530f81ffb75265ebfe3b35b458a1192d2
Zakaria Yaris <info@zakariya.me>
2023-05-25 14:47:46 +0100

    added to display who recommended the song

--- src/model/posts.js
+++ src/model/posts.js
@@ -1,12 +1,12 @@
-const db = require("../database/db");
+const db = require('../database/db');
 
-const get_all_posts = db.prepare(/*sql*/`
-SELECT 
-  p.id AS post_id, 
-  p.artist, 
+const get_all_posts = db.prepare(/*sql*/ `
+SELECT
+  p.id AS post_id,
+  p.artist,
   p.song,
   p.spotify_url,
-  u.name AS user_name, 
+  u.name AS user_name,
   p.posted_at
 FROM posts AS p
 JOIN users AS u ON p.user_id = u.id
@@ -14,12 +14,10 @@ ORDER BY p.posted_at DESC
 `);
 
 function getAllPosts() {
-  const posts = get_all_posts.all();
-  console.log(posts);
-  return posts;
+  return get_all_posts.all();
 }
 
-const create_post = db.prepare(/*sql*/`
+const create_post = db.prepare(/*sql*/ `
   INSERT INTO posts (
     user_id,
     artist,
@@ -33,20 +31,20 @@ const create_post = db.prepare(/*sql*/`
   RETURNING id
   `);
 
-function createPost({user_id, artist, song, spotify_url}) {
+function createPost({ user_id, artist, song, spotify_url }) {
   return create_post.get({ user_id, artist, song, spotify_url });
 }
 
-const delete_post = db.prepare(/*sql*/`
+const delete_post = db.prepare(/*sql*/ `
   DELETE FROM posts
   WHERE id = $post_id
 `);
 
 function deletePost(post_id) {
- delete_post.run({ post_id });
+  delete_post.run({ post_id });
 }
 
-const clear_ratings = db.prepare(/*sql*/`
+const clear_ratings = db.prepare(/*sql*/ `
   DELETE FROM ratings
 `);
 
